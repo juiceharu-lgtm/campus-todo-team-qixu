8a0eeb4ee4e477852d8e0f7e669c285a769fcee9
ashy069 <36348139@qq.com>
2026-09-23 23:32:20 +0800

    test: specify task completion rules

--- src/test/java/edu/hbuas/campustodo/service/TaskServiceTest.java
+++ src/test/java/edu/hbuas/campustodo/service/TaskServiceTest.java
@@ -1,17 +1,13 @@
 package edu.hbuas.campustodo.service;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class TaskServiceTest {
 
     @Test
     void shouldAddTask() {
         TaskService service = new TaskService();
-
         var task = service.addTask("完成需求评审");
 
         assertEquals(1L, task.getId());
@@ -23,8 +19,35 @@ class TaskServiceTest {
     @Test
     void shouldRejectBlankTitle() {
         TaskService service = new TaskService();
-
         assertThrows(IllegalArgumentException.class,
-                () -> service.addTask("   "));
+            () -> service.addTask(" "));
+    }
+
+    // 1.正常完成任务
+    @Test
+    void shouldCompleteTaskSuccess() {
+        TaskService service = new TaskService();
+        var task = service.addTask("写软件工程作业");
+        service.completeTask(task.getId());
+        assertTrue(task.isCompleted());
+    }
+
+    //2.不存在任务id抛出异常
+    @Test
+    void shouldThrowWhenIdNotExist() {
+        TaskService service = new TaskService();
+        assertThrows(IllegalArgumentException.class,
+            () -> service.completeTask(999L));
+    }
+
+    //3.重复完成已完成任务抛出异常（实验必做！）
+    @Test
+    void shouldThrowWhenCompleteAlreadyDoneTask() {
+        TaskService service = new TaskService();
+        var task = service.addTask("复习考试");
+        service.completeTask(task.getId());
+        //第二次完成，需要抛出IllegalStateException
+        assertThrows(IllegalStateException.class,
+            () -> service.completeTask(task.getId()));
     }
 }
