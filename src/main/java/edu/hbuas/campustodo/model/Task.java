a5cc5c59733bbd76ec3d335069025a1389a7d841
SophiaYuan11701 <2806152307@qq.com>
2026-09-24 01:48:32 +0800

    Add files via upload

--- src/main/java/edu/hbuas/campustodo/model/Task.java
+++ src/main/java/edu/hbuas/campustodo/model/Task.java
@@ -1,20 +1,19 @@
 package edu.hbuas.campustodo.model;
-
 import java.util.Objects;
 
 /**
  * 校园待办任务。
  */
 public class Task {
+    //实验#1新增：优先级枚举
     public enum Priority {
-        HIGH,
-        MEDIUM,
-        LOW
+        HIGH, MEDIUM, LOW
     }
 
     private final long id;
     private final String title;
     private boolean completed;
+    //新增优先级字段
     private Priority priority;
 
     public Task(long id, String title) {
@@ -26,31 +25,29 @@ public class Task {
         }
         this.id = id;
         this.title = title.trim();
+        //新建任务默认MEDIUM
         this.priority = Priority.MEDIUM;
     }
 
     public long getId() {
         return id;
     }
-
     public String getTitle() {
         return title;
     }
-
     public boolean isCompleted() {
         return completed;
     }
+    public void complete() {
+        completed = true;
+    }
 
+    //新增getter、setter
     public Priority getPriority() {
         return priority;
     }
-
     public void setPriority(Priority priority) {
         this.priority = priority;
-    }
-
-    public void complete() {
-        completed = true;
     }
 
     @Override
@@ -63,7 +60,6 @@ public class Task {
         }
         return id == task.id;
     }
-
     @Override
     public int hashCode() {
         return Objects.hash(id);
